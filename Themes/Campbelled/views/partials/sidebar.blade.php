49ea47f5b3a4f7bb62daeb0026447ee882a2eb49
Nicolas Widart <n.widart@gmail.com>
2015-12-24 16:27:38 +0100

    Adding about section

--- Themes/Campbelled/views/partials/sidebar.blade.php
+++ Themes/Campbelled/views/partials/sidebar.blade.php
@@ -5,6 +5,14 @@
             <p>This site re-groups all friendly comments from Graham Campbell.</p>
         </header>
     </section>
+    <section class="blurb">
+        <h2>About</h2>
+        <p>
+            This site was built to show how rude and inappropriate the replies are on the laravel repositories. With the hope that it will open the eyes of above mentioned author to take action, and make the community worthwhile. These kind of replies don't motivate people to contribute to the laravel framework, on the contrary.
+            <br>
+            Look at the <a href="https://github.com/symfony/symfony/pulls?q=is%3Apr+is%3Aclosed" target="_blank">closed Symfony pull-requests</a>, to get an idea of what a nice and welcoming community looks like.
+        </p>
+    </section>
     <section id="footer">
         <ul class="icons">
             <li><a href="#" class="fa-github"><span class="label">Github</span></a></li>
