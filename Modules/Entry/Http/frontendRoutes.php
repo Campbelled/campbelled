6a4d0cbb87b02fa8525a28f8f4ae82482c3b42d7
Nicolas Widart <n.widart@gmail.com>
2015-12-24 16:33:04 +0100

    Loading front end route

--- Modules/Entry/Http/frontendRoutes.php
+++ Modules/Entry/Http/frontendRoutes.php
@@ -1,1 +1,6 @@
 <?php
+
+$router->bind('entries', function ($id) {
+    return app(\Modules\Entry\Repositories\EntryRepository::class)->find($id);
+});
+get('entry/{entries}', ['uses' => 'Frontend\EntryController@show', 'as' => 'entry.show']);
