0917bfaee23bdb628a6331fe5c8a2dc5f808bffd
Nicolas Widart <n.widart@gmail.com>
2015-12-25 06:13:42 +0100

    Redirect to laravel site

--- Modules/Entry/Http/frontendRoutes.php
+++ Modules/Entry/Http/frontendRoutes.php
@@ -4,3 +4,6 @@ $router->bind('entries', function ($id) {
     return app(\Modules\Entry\Repositories\EntryRepository::class)->find($id);
 });
 get('entry/{entries}', ['uses' => 'Frontend\EntryController@show', 'as' => 'entry.show']);
+$router->get('/', function () {
+    return redirect('http://laravel.com');
+});
